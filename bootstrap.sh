959aaa657f1717c62f5259b1b95f8b45be644a07
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2026-08-27 16:58:04 -0700

    Add telnet to applications list
    Telnet, though insecure for usage over the network, is the IPC protocol
    of choice for the JLink program.

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,8 +54,8 @@ do_root() {
         audacity betterdisplay cmake claude claude-code fd fish gh gimp     \
         git-lfs google-chrome htop macchina macs-fan-control markedit       \
         neovim netron ninja nrf-connect parallel pipx pyenv qdirstat        \
-        segger-jlink tailscale-app raspberry-pi-imager ripgrep rsync rust   \
-        rustup windows-app wireshark-app yazi zotero
+        segger-jlink tailscale-app telnet raspberry-pi-imager ripgrep rsync \
+        rust rustup windows-app wireshark-app yazi zotero
 
     # declare brew-managed rust to rustup and make it the default
     __source_rustup &&                                              \
